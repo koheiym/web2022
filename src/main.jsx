7d9a23991d8d92376a805ed5fc13916476ef643b
koheiym <91835891+koheiym@users.noreply.github.com>
2022-12-16 16:33:06 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App" assert{type:json};
 import "bulma/css/bulma.css";
 
 createRoot(document.querySelector("#content")).render(<App />);
